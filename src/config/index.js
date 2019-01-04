6d66bc83a225546963b3c20248e0ce1f71486b75
wafaa <wafaa.abudawod@gmail.com>
2019-01-04 21:04:30 +0200

    make it better

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ export default {
   // "port": 3005,
   // "mongoUrl": "mongodb://localhost:27017/chat-api",
   "port": process.env.PORT,
-  "mongoUrl": "mongodb://<dbuser>:<dbpassword>@ds123434.mlab.com:23434/chattychatchat",
+  "mongoUrl": "mongodb://wafaa:wafaa@987@ds123434.mlab.com:23434/chattychatchat",
   "bodyLimit": "100kb"
 }
